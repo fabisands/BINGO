6bfe748022d59c3333e3ddfa112857bf2ca6bcfb
fabisands <f.arenas0294@gmail.com>
2023-10-08 12:14:44 -0500

    🚚

--- BINGO game 2.py
+++ BINGO game 2.py
@@ -1,8 +1,5 @@
 import random
-import tkinter as tk
-
-# Initialize a variable to keep track of the code number
-code_number = 0
+import msvcrt  # Import msvcrt for keyboard input on Windows
 
 # Function to generate random Bingo numbers
 def generate_bingo_numbers():
@@ -19,23 +16,17 @@ def generate_bingo_numbers():
             generated_numbers.add(num)
             random_numbers.append(f"{num:02d}")
 
-    # Clear the existing text in the bingo_label
-    bingo_label.config(text="")
-
     # Display the letters BINGO above each column
-    bingo_label.config(text="B    I    N    G    O")
+    print("B   I   N   G   O")
 
     # Display the generated numbers in 5 rows of 5 numbers each
     for i in range(5):
         row = random_numbers[i * 5: (i + 1) * 5]
         row_str = "  ".join(row)
-        bingo_label.config(text=bingo_label.cget("text") + f"\n{row_str}")
+        print(row_str)
 
 # Function to generate a random code
 def generate_random_code():
-    global code_number  # Use the global code_number variable
-    code_number += 1  # Increment the code_number
-
     # Define the available letters and numbers
     letters = ['B', 'I', 'N', 'G', 'O']
     min_number = 1
@@ -51,34 +42,19 @@ def generate_random_code():
     random_number_str = str(random_number).zfill(2)
 
     # Create the random code by combining the letter and number
-    random_code = f"Code Number {code_number}: {random_letter}{random_number_str}"
+    random_code = f"{random_letter}{random_number_str}"
 
-    # Clear the existing text in the code_label
-    code_label.config(text=code_label.cget("text") + f"\n{random_code}")
+    # Display the random code
+    print(random_code)
 
-# Create the main window
-window = tk.Tk()
-window.title("Random Code Generator")
+# Call the function to generate Bingo numbers once
+generate_bingo_numbers()
 
-# Create a frame to hold the buttons and position it at the top of the window
-button_frame = tk.Frame(window)
-button_frame.pack()
-
-# Create a button to generate Bingo numbers without "Generate" in the label
-bingo_button = tk.Button(button_frame, text="BINGO Numbers", command=generate_bingo_numbers, font=("Helvetica", 16))
-bingo_button.pack(side=tk.LEFT)
-
-# Create a button to generate random codes without "Generate" in the label
-code_button = tk.Button(button_frame, text="Random Code", command=generate_random_code, font=("Helvetica", 16))
-code_button.pack(side=tk.LEFT)
-
-# Create a label to display BINGO numbers
-bingo_label = tk.Label(window, text="", padx=20, pady=10, font=("Helvetica", 24))
-bingo_label.pack()
-
-# Create a label to display random codes
-code_label = tk.Label(window, text="", padx=20, pady=10, font=("Helvetica", 24))
-code_label.pack()
-
-# Run the Tkinter main loop
-window.mainloop()
+# Allow the user to generate random codes by pressing Enter and terminate on any other key press
+print("Press Enter to generate a random code, or any other key to exit...")
+while True:
+    key = msvcrt.getch()  # Get a key press
+    if key == b'\r':  # Enter key was pressed
+        generate_random_code()
+    else:
+        break  # Exit the loop if any other key
